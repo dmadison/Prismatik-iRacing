a150d325a784cfcefa208e1707c53e24809b3f2f
David Madison <dmadison@users.noreply.github.com>
2017-11-22 16:09:45 -0500

    Add License (#2)
    * Added license
    
    * Added license notices to source
    
    * Added license to README

--- Prismatik-iRacing.py
+++ Prismatik-iRacing.py
@@ -1,3 +1,24 @@
+#
+# Project     Prismatik - iRacing Plugin
+# @author     David Madison
+# @link       github.com/dmadison/Prismatik-iRacing
+# @license    GPLv3 - Copyright (c) 2017 David Madison
+#
+# This file is part of the Prismatik - iRacing Plugin.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import irsdk
 import time
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # iRacing Plugin for Prismatik
-
 This plugin allows you to visualize data from iRacing with an ambilight.
+
+## License
+This plugin is licensed under the terms of the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html).
